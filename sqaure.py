28fd5937254c5bf8104d2eb81b59652846ad3792
HD88-max <hardikdhallam@gmail.com>
2025-02-16 15:34:17 +0000

    Add files via upload

---
+++ sqaure.py
@@ -0,0 +1,9 @@
+import turtle
+x = turtle.Screen()
+#turtle.Screen().setup(500,500)
+pen = turtle.Turtle()
+while True:
+    for i in range(4):
+        pen.forward(20)
+        pen.left(90)
+turtle.done()
